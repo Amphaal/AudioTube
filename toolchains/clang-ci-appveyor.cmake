d75495b40c84fd67c1a9bdba8cd795ebef2d6092
Guillaume VARA <guillaume.vara@gmail.com>
2020-03-31 00:26:33 +0200

    another try

--- toolchains/clang-ci-appveyor.cmake
+++ toolchains/clang-ci-appveyor.cmake
@@ -5,8 +5,8 @@ SET (CMAKE_C_COMPILER               "clang")
 SET (CMAKE_C_FLAGS                  "-fuse-ld=lld -Wall")
 SET (CMAKE_C_FLAGS_RELEASE          "-O3 -DNDEBUG")
 
-SET (CMAKE_EXE_LINKER_FLAGS_INIT    "-Xlinker /implib:libcmt.lib")
-SET (CMAKE_SHARED_LINKER_FLAGS_INIT ${CMAKE_EXE_LINKER_FLAGS_INIT})
+# SET (CMAKE_EXE_LINKER_FLAGS_INIT    "-Xlinker /implib:libcmt.lib")
+# SET (CMAKE_SHARED_LINKER_FLAGS_INIT ${CMAKE_EXE_LINKER_FLAGS_INIT})
 
 SET (CMAKE_CXX_COMPILER             "clang++")
 SET (CMAKE_CXX_FLAGS                "-fuse-ld=lld -Wall")
