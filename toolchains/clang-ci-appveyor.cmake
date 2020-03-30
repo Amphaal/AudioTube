e1f28cde560e514276e72fbe4ce9267510a811a2
Guillaume VARA <guillaume.vara@gmail.com>
2020-03-31 00:23:20 +0200

    silly

--- toolchains/clang-ci-appveyor.cmake
+++ toolchains/clang-ci-appveyor.cmake
@@ -5,7 +5,7 @@ SET (CMAKE_C_COMPILER               "clang")
 SET (CMAKE_C_FLAGS                  "-fuse-ld=lld -Wall")
 SET (CMAKE_C_FLAGS_RELEASE          "-O3 -DNDEBUG")
 
-SET (CMAKE_EXE_LINKER_FLAGS_INIT    "-Xlinker /implib:libcmt.lib"
+SET (CMAKE_EXE_LINKER_FLAGS_INIT    "-Xlinker /implib:libcmt.lib")
 SET (CMAKE_SHARED_LINKER_FLAGS_INIT ${CMAKE_EXE_LINKER_FLAGS_INIT})
 
 SET (CMAKE_CXX_COMPILER             "clang++")
