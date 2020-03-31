fa68a2dfbb0585a41371af6351bdee4004a46aff
Guillaume VARA <guillaume.vara@gmail.com>
2020-03-31 12:23:15 +0200

    force target

--- toolchains/clang-ci-appveyor.cmake
+++ toolchains/clang-ci-appveyor.cmake
@@ -2,9 +2,9 @@ SET(CMAKE_BUILD_TYPE Release)
 SET(Qt5_DIR "C:/Qt/5.14/msvc2017_64/lib/cmake/Qt5") 
 
 SET (CMAKE_C_COMPILER             "clang")
-SET (CMAKE_C_FLAGS                "-fuse-ld=lld -Wall")
+SET (CMAKE_C_FLAGS                "-fuse-ld=lld -target x86_64-pc-windows-msvc -Wall")
 SET (CMAKE_C_FLAGS_RELEASE        "-O3 -DNDEBUG")
 
 SET (CMAKE_CXX_COMPILER             "clang++")
-SET (CMAKE_CXX_FLAGS                "-fuse-ld=lld -Wall")
+SET (CMAKE_CXX_FLAGS                "-fuse-ld=lld -target x86_64-pc-windows-msvc -Wall")
 SET (CMAKE_CXX_FLAGS_RELEASE        "-O3 -DNDEBUG")
