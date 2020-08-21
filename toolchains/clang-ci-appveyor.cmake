4e8a0f78724766ec66626eb644ece3215a4171c0
Amphaal <guillaume.vara@gmail.com>
2020-08-21 14:24:08 +0200

    w

--- toolchains/clang-ci-appveyor.cmake
+++ toolchains/clang-ci-appveyor.cmake
@@ -1,4 +1,4 @@
 SET(CMAKE_BUILD_TYPE Release)
 
-SET (CMAKE_C_COMPILER               "clang-cl")	
-SET (CMAKE_CXX_COMPILER             "clang-cl")
+SET (CMAKE_C_COMPILER               "clang")	
+SET (CMAKE_CXX_COMPILER             "clang++")
