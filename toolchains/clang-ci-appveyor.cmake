b2a82c05c02e0db2846f26d5a6527ffff7403c37
Amphaal <guillaume.vara@gmail.com>
2020-08-21 14:22:31 +0200

    wip

--- toolchains/clang-ci-appveyor.cmake
+++ toolchains/clang-ci-appveyor.cmake
@@ -1,6 +1,4 @@
 SET(CMAKE_BUILD_TYPE Release)
 
 SET (CMAKE_C_COMPILER               "clang-cl")	
-SET (CMAKE_CXX_COMPILER             "clang++")
-
-SET (CMAKE_CXX_FLAGS                "-D_WIN32_WINNT=0x0601")
+SET (CMAKE_CXX_COMPILER             "clang-cl")
