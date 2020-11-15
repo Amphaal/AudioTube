ae5a1fb1cedfa4b8a85c7bfadd4a6c67108cc44b
Amphaal <guillaume.vara@gmail.com>
2020-11-15 21:20:23 +0100

    fix promise-cpp

--- toolchains/msys.cmake
+++ toolchains/msys.cmake
@@ -4,11 +4,12 @@ SET(CMAKE_RANLIB                    "llvm-ranlib")
 SET(CMAKE_AR                        "llvm-ar")
 
 SET (CMAKE_C_COMPILER               "clang")
-SET (CMAKE_C_FLAGS                  "-fuse-ld=lld -Wall")
-SET (CMAKE_C_FLAGS_DEBUG            "-O0 -g")
+SET (CMAKE_CXX_COMPILER             "clang++")
 
 SET (CMAKE_EXE_LINKER_FLAGS         "-Wl,/force:multiple")
 
-SET (CMAKE_CXX_COMPILER             "clang++")
-SET (CMAKE_CXX_FLAGS                "-fuse-ld=lld -Wall")
-SET (CMAKE_CXX_FLAGS_DEBUG          "-O0 -g")
+SET (CMAKE_C_FLAGS                  "-fuse-ld=lld -Wall")
+SET (CMAKE_C_FLAGS_DEBUG            "-O0 -g")
+
+SET (CMAKE_CXX_FLAGS                ${CMAKE_C_FLAGS})
+SET (CMAKE_CXX_FLAGS_DEBUG          ${CMAKE_C_FLAGS_DEBUG})
