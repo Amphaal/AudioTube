e5252fd61cb72b098e3ccc3d519f44380e5dd7fa
Amphaal <guillaume.vara@gmail.com>
2021-10-03 12:47:31 +0200

    Update Copyright

--- tests/sub/url.hpp
+++ tests/sub/url.hpp
@@ -1,6 +1,6 @@
 // AudioTube C++
 // C++ fork based on https://github.com/Tyrrrz/YoutubeExplode
-// Copyright (C) 2019-2020 Guillaume Vara
+// Copyright (C) 2019-2021 Guillaume Vara
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
