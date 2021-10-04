ce9f3e08804d921955cd7cdad7076966875ce801
Amphaal <guillaume.vara@gmail.com>
2021-10-04 13:07:48 +0200

    update MacOS build env

--- toolchains/osx.cmake
+++ toolchains/osx.cmake
@@ -1,13 +1,13 @@
+SET(CMAKE_BUILD_TYPE Debug)
+
+SET (CMAKE_CXX_FLAGS "-Wno-deprecated-declarations") #prevent asio's std::allocator<void> depreciation warnings
+
 SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH ON) #prevent linking errors once app shared
 SET(CMAKE_MACOSX_RPATH ON)
-
-#set(CMAKE_OSX_SYSROOT /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk)
-SET(CMAKE_OSX_DEPLOYMENT_TARGET 10.13)
+SET(CMAKE_OSX_DEPLOYMENT_TARGET 10.15)
 
 list(APPEND CMAKE_PREFIX_PATH 
     "/usr/local/opt"
 )
 
-set(OPENSSL_ROOT_DIR "/usr/local/Cellar/openssl@1.1/1.1.1f")
-
-SET(CMAKE_BUILD_TYPE Debug)
+set(OPENSSL_ROOT_DIR "/usr/local/Cellar/openssl@3/*")
